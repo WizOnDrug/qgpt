77235b7616df5b69c00df8abb76ebc15db242889
Wiz <im.not.ali@outlook.com>
2024-01-31 07:25:54 +0330

    Configure Layout With SideBar and Header Component and Set Props Children for Another Components

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,24 @@
+import Header from "./Header";
+import Sidebar from "../module/Sidebar";
+
+import { FC, ReactNode } from 'react'
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex w-[1440px] h-[740px] p-5 gap-5">
+      <Sidebar />
+      <div className="w-[692px] h-[700px] rounded-md border border-slate-200 flex-col justify-start gap-[12px] inline-flex">
+        <Header />
+        <div>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Layout;
