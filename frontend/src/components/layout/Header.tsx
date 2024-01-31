349b17840f6f18ccdbee578c989846b63e710954
Wiz <im.not.ali@outlook.com>
2024-01-31 07:23:35 +0330

    Add Header Component

---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <div className="w-[690px] h-[45px] p-3 bg-slate-100 border-b border-slate-200 items-center gap-2.5 inline-flex">
+        <div className="text-right text-gray-900 text-sm leading-[21px]">درخواست پشتیبانی</div>
+        </div>
+    )
+}
