04e57a7910fdd5f39489a7dc7d251ddc3777055c
Wiz <im.not.ali@outlook.com>
2024-02-01 04:06:35 +0330

    Debug Layout Screen Size

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -9,9 +9,9 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex w-[1440px] h-[740px] p-5 gap-5">
+    <div className="flex w-[1440px]  p-5 gap-5">
       <Sidebar />
-      <div className="w-[692px] h-[700px] rounded-md border border-slate-200 flex-col justify-start gap-[12px] inline-flex">
+      <div className="w-[692px] rounded-md border border-slate-200 flex-col justify-start gap-[12px] inline-flex">
         <Header />
         <div>
           {children}
